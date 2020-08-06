8bc8a19e0937b86462d9d246930f3d66f2cb1709
oseo3361 <seohj8028@hotmail.com>
2020-08-06 16:16:18 -0400

    fixed to get photos saved in subcategory table

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -247,7 +247,7 @@ exports.renderEventPage = (req, res) => {
     let year_num = req.params.year_num;
     let allEvents = `select e.id, e.event_title, e.event_location, e.date_from, e.date_to, year(e.date_from) as year_num, s.photo_link from cc_event e join cc_event_subcategory s where e.id = s.cc_event_id and year(e.date_from) = ${pool.escape(year_num)} group by e.id order by e.date_from desc`
     let newerEvents = `select e.id from cc_event e join cc_photo p where e.id = p.cc_event_id and year(e.date_from) = ${pool.escape(year_num)} group by e.id order by e.date_from desc`;
-    let subcategories = `select s.id, s.cc_event_id, s.event_sub_category from cc_event e join cc_event_subcategory s where e.id = s.cc_event_id and year(e.date_from) = ${pool.escape(year_num)}`;
+    let subcategories = `select s.id, s.cc_event_id, s.event_sub_category, s.photo_link from cc_event e join cc_event_subcategory s where e.id = s.cc_event_id and year(e.date_from) = ${pool.escape(year_num)}`;
     //execute query
     pool.query(allEvents, (err, result) => {
         if (err) {
@@ -277,22 +277,29 @@ exports.renderGalleryPage = (req, res) => {
     let year_num = req.params.year_num;
     let event_id = req.params.event_id;
     let id = req.params.id;
-    let sql = `select * from cc_photo p join cc_event_subcategory s join cc_event e where p.cc_event_subcategory_id = s.id and p.cc_event_id = e.id and year(e.date_from) = ${pool.escape(year_num)} and e.id = ${pool.escape(event_id)} and s.id = ${pool.escape(id)}`;
+    let getSelectedEvent = `select * from cc_event e join cc_event_subcategory s where e.id = s.cc_event_id and year(e.date_from) = ${pool.escape(year_num)} and e.id = ${pool.escape(event_id)} and s.id = ${pool.escape(id)}`;
+    let getPhotos = `select * from cc_photo p join cc_event e where p.cc_event_id = e.id and year(e.date_from) = ${pool.escape(year_num)} and e.id = ${pool.escape(event_id)} and p.cc_event_subcategory_id = ${pool.escape(id)}`;
     let sql2 = `select s.id, s.cc_event_id, s.event_sub_category, e.event_title, year(e.date_from) as year_num from cc_event_subcategory s join cc_event e where s.cc_event_id = e.id and year(e.date_from) = ${pool.escape(year_num)} and e.id = ${pool.escape(event_id)}`;
 
     //execute query
-    pool.query(sql, (err, result) => {
+    pool.query(getSelectedEvent, (err, result) => {
         if (err) {
             res.redirect('/');
         }
-        pool.query(sql2, (err2, result2) => {
-            if (err2) {
+        pool.query(getPhotos, (err, result2) => {
+            if (err) {
                 res.redirect('/');
             }
-            res.render('gallery.ejs', {
-                title: 'Canadian Cyclist',
-                posts: result,
-                types: result2,
+            pool.query(sql2, (err2, result3) => {
+                if (err2) {
+                    res.redirect('/');
+                }
+                res.render('gallery.ejs', {
+                    title: 'Canadian Cyclist',
+                    posts: result,
+                    photos: result2,
+                    types: result3,
+                });
             });
         });
     });
